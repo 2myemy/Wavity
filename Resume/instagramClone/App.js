7f15624012945d248cff9285aea5f05b3a18fb5d
Myungyeon Lee <myungyeonlee@Myungyeonui-MacBookPro.local>
2019-11-28 01:33:05 +0900

    create app

---
+++ Resume/instagramClone/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+// import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import {createAppContainer } from 'react-navigation';
+import MainScreen from './components/MainScreen';
+
+const AppStackNavigator = createStackNavigator({
+  Main:{
+    screen: MainScreen // MainScreen 컴포넌트를 네비게이터에 등록
+  }
+});
+
+export default createAppContainer(AppStackNavigator);
