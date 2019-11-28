6c7124dacace768b71094bd9c742df26a9fa9992
Myungyeon Lee <myungyeonlee@Myungyeonui-MacBookPro.local>
2019-11-29 08:40:35 +0900

    Improved instagramClone app

--- Resume/instagramClone/App.js
+++ Resume/instagramClone/App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 // import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import {createAppContainer } from 'react-navigation';
-import MainScreen from './components/MainScreen';
+import MainScreen from './Components/MainScreen';
 
 const AppStackNavigator = createStackNavigator({
   Main:{
--- Resume/instagramClone/components/MainScreen.js
+++ Resume/instagramClone/components/MainScreen.js
@@ -1,6 +1,43 @@
 import React, { Component } from 'react';
 import { StyleSheet, Platform, View, Text } from 'react-native';
 import { Icon } from 'native-base'; // 추가된 코드
+import { createAppContainer } from 'react-navigation'; 
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs'; 
+// 하단 탭에 들어갈 컴포넌트들
+import HomeTab from './AppTabNavigator/HomeTab'
+import SearchTab from './AppTabNavigator/SearchTab'
+import AddMediaTab from './AppTabNavigator/AddMediaTab'
+import LikesTab from './AppTabNavigator/LikesTab'
+import ProfileTab from './AppTabNavigator/ProfileTab'
+
+const AppTabNavigator = createMaterialTopTabNavigator({
+  HomeTab: { screen: HomeTab },
+  SearchTab: { screen: SearchTab },
+  AddMediaTab: { screen: AddMediaTab },
+  LikesTab: { screen: LikesTab },
+  ProfileTab: { screen: ProfileTab }
+}, {
+  animationEnabled: true,
+  swipeEnabled: true,
+  tabBarPosition: "bottom",
+  tabBarOptions: {
+    style: {
+      ...Platform.select({
+        ios:{
+          backgroundColor:'white',
+        }
+      })
+    },
+    iconStyle: { height: 100 },
+    activeTintColor: '#000',
+    inactiveTintColor: '#d1cece',
+    upperCaseLabel: false,
+    showLabel: false,
+    showIcon: true,
+  }
+});
+
+const AppTabContainet = createAppContainer(AppTabNavigator);
 
 export default class MainScreen extends Component {
 
@@ -11,11 +48,12 @@ export default class MainScreen extends Component {
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>MainScreen</Text>
-      </View>
-    );
+    // return (
+    //   <View style={styles.container}>
+    //     <Text>MainScreen</Text>
+    //   </View>
+    // );
+    return <AppTabContainet/>; // AppTabContainet 컴포넌트를 리턴한다.
   }
 }
 
